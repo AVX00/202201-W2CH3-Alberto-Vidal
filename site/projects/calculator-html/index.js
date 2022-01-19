b4814161ce380965aa522eeda981fcd51bffd364
Alberto Vidal <alberto_vidalm@protonmail.com>
2022-01-19 23:18:54 +0100

    fixed eslint

--- site/projects/calculator-html/index.js
+++ site/projects/calculator-html/index.js
@@ -6,25 +6,18 @@ function clearScreen() {
 }
 
 function clearMem() {
-  
   while (mem.length) {
     mem.shift();
   }
 }
 
 function writeNum(num) {
-
-    
-    
   if (num === "." && visor.innerHTML.toString().includes(".")) {
     console.log("ya hay un punto");
+  } else if (visor.innerHTML === "0") {
+    visor.innerHTML = num;
   } else {
-      if (visor.innerHTML==='0') {
-        visor.innerHTML = num;
-        }else{
-            visor.innerHTML += num;
-        }
-    
+    visor.innerHTML += num;
   }
 }
 
@@ -43,60 +36,56 @@ function writeRes(res) {
 }
 
 function calc() {
-  let r=0,aux=0;
+  let r = 0;
+  let aux = 0;
   if (visor.innerHTML.length) {
     mem.push(Number(visor.innerHTML));
     clearScreen();
-do {
-    if (r===0) {
+    do {
+      if (r === 0) {
         switch (mem[1]) {
-        case "+":
-          r += mem[0] + mem[2 ];
+          case "+":
+            r += mem[0] + mem[2];
 
-          break;
-        case "-":
-          r += mem[0 ] - mem[2 ];
+            break;
+          case "-":
+            r += mem[0] - mem[2];
 
-          break;
-        case "*":
-          r += mem[0 ] * mem[2];
+            break;
+          case "*":
+            r += mem[0] * mem[2];
 
-          break;
-       
-        default:
+            break;
+
+          default:
             r += mem[0] / mem[2];
-          break;
-      }
-    } else {
+            break;
+        }
+      } else {
         console.log(mem);
-        switch (mem[1+aux*2]) {
-            case "+":
-              r += mem[2+aux*2];
-    
-              break;
-            case "-":
-              r -=  mem[2 +aux*2];
-    
-              break;
-            case "*":
-              r *= mem[2+aux*2];
-    
-              break;
-           
-    
-            default:
-                r /=  mem[2+aux*2];
-              break;
-          }
-    }
-    aux++
-} while (aux<mem.length/3);
+        switch (mem[1 + aux * 2]) {
+          case "+":
+            r += mem[2 + aux * 2];
 
-    
-      
-    
+            break;
+          case "-":
+            r -= mem[2 + aux * 2];
+
+            break;
+          case "*":
+            r *= mem[2 + aux * 2];
+
+            break;
+
+          default:
+            r /= mem[2 + aux * 2];
+            break;
+        }
+      }
+      aux++;
+    } while (aux < mem.length / 3);
+
     writeRes(r);
-    clearMem()
-    
+    clearMem();
   }
 }
